a64eef74654452dd02ee56a047a0b34ddb9be329
XndroidDev <xndroiddev@gmail.com>
2018-10-01 16:39:44 +0800

    fix auto start fail

--- app/src/main/java/net/xndroid/AutoStart.java
+++ app/src/main/java/net/xndroid/AutoStart.java
@@ -5,15 +5,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.preference.PreferenceManager;
 
-import static net.xndroid.AppModel.sContext;
-
 public class AutoStart extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
         // TODO: This method is called when the BroadcastReceiver is receiving
         if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
-            if (PreferenceManager.getDefaultSharedPreferences(sContext)
+            if (PreferenceManager.getDefaultSharedPreferences(context)
                     .getBoolean(AppModel.PRE_AUTO_START, false)){
                 AppModel.appInit(context);
             }
